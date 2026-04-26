b11db1a89e05804493eaaad0dfe65aafab07db25
Brian Bassett <bbassett1276@gmail.com>
2026-04-26 14:37:22 -0700

    Initial implementation of command line parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use human_units::Size;
+use std::path::PathBuf;
+
+/// Split a set of files into fixed size chunks and optionally transfer them to a remote host.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CliArgs {
+    /// OpenSSH private key file to authenticate remote SSH connections
+    #[arg(short)]
+    identity_file: Option<PathBuf>,
+
+    /// size of the split chunks
+    #[arg(short, default_value = "3M")]
+    size: Size,
+
+    /// Files to split and transfer
+    files: Vec<PathBuf>,
+
+    /// Destination to transfer split chunks to
+    #[arg(last = true)]
+    dest: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+    println!("Arguments: {:?}", args);
 }
